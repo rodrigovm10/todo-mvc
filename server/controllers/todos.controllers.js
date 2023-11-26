0c7da6cc077dea78d15bba3168c4b73081c8b4c5
rodrigovm10 <rv0611200369@gmail.com>
2023-11-26 01:35:44 -0600

    Creacion front

--- server/controllers/todos.controllers.js
+++ server/controllers/todos.controllers.js
@@ -1,39 +1,51 @@
 import Todo from '../models/Todo.js'
 
 export const getTodos = async (req, res) => {
-  const todos = await Todo.find()
-  res.send(todos)
+  try {
+    const todos = await Todo.find()
+    res.send(todos)
+  } catch (error) {
+    return res.status(500).json(error.message)
+  }
 }
 
 export const createTodos = async (req, res) => {
-  const { name, description, date } = req.body
-
-  const newTodo = new Todo({ name, description, date })
-
-  await newTodo.save()
-  return res.json(newTodo)
+  try {
+    const { name, description, date } = req.body
+    const newTodo = new Todo({ name, description, date })
+    await newTodo.save()
+    return res.json(newTodo)
+  } catch (error) {
+    return res.status(500).json(error.message)
+  }
 }
 
 export const updateTodo = async (req, res) => {
-  const todoUpdated = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true })
-
-  if (!todoUpdated) return res.sendStatus(404)
-
-  return res.sendStatus(204)
+  try {
+    const todoUpdated = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    if (!todoUpdated) return res.sendStatus(404)
+    return res.send(todoUpdated)
+  } catch (error) {
+    return res.status(500).json(error.message)
+  }
 }
 
 export const deleteTodo = async (req, res) => {
-  const todoDeleted = await Todo.findByIdAndDelete(req.params.id)
-
-  if (!todoDeleted) return res.sendStatus(404)
-
-  return res.sendStatus(204)
+  try {
+    const todoDeleted = await Todo.findByIdAndDelete(req.params.id)
+    if (!todoDeleted) return res.sendStatus(404)
+    return res.sendStatus(204)
+  } catch (error) {
+    return res.status(500).json(error.message)
+  }
 }
 
 export const getTodo = async (req, res) => {
-  const todo = Todo.findById(req.params.id)
-
-  if (!todo) return res.sendStatus(404)
-
-  return res.json(todo)
+  try {
+    const todo = Todo.findById(req.params.id)
+    if (!todo) return res.sendStatus(404)
+    return res.json(todo)
+  } catch (error) {
+    return res.status(500).json(error.message)
+  }
 }
